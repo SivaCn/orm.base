e48225fbd2c09ee04fa2f79457e4ac9af7f51995
Siva Cn <cnsiva@protonmail.com>
2023-01-29 03:46:48 +0530

    directory structuring

--- setup.py
+++ setup.py
@@ -15,50 +15,41 @@ https://github.com/pypa/sampleproject
 """
 
 # To use a consistent encoding
-from os import path
+import os
+import toml
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
+
+config_file_path = os.path.join(here, 'pyproject.toml')
+with open(config_file_path, 'r') as fp:
+    pyproject_text = fp.read()
+
+project = toml.loads(pyproject_text)['project']
 
 setup(
-    name='orm.base',
-    version='1.0.0',
-    description='',
+    name=project['name'],
+    version=project['version'],
+    description=project['description'],
     author='Siva Cn',
     author_email='cnsiva@protonmail.com',
-    project_urls={
-        'Source': 'https://github.com/SivaCn/orm.base',
-    },
+    project_urls=project['urls'],
     license='MIT',
     platforms='any',
     # Classifiers help users find your project by categorizing it.
     #
     # For a list of valid classifiers, see
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        "Operating System :: OS Independent",
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
+    classifiers=project['classifiers'],
 
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     namespace_packages=['orm', 'orm.base'],
 
     # Install dependencies
     python_requires=">=2.7",
-    install_requires=[
-        "sqlalchemy>=1.2.19",
-    ],
+    install_requires=project['dependencies'],
 
     # Console script entry points
     entry_points={},
